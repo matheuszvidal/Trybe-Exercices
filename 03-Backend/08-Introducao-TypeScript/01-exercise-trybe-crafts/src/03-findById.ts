42c574e2db8915c4fde1451be2a12099a540bf1b
Matheus Vidal <matheusvidalpro@gmail.com>
2023-06-21 23:11:48 -0400

    feat: requirement 03 - done

---
+++ 03-Backend/08-Introducao-TypeScript/01-exercise-trybe-crafts/src/03-findById.ts
@@ -0,0 +1,31 @@
+import { Product } from "./02-insertProducts";
+
+const products = [
+  {
+    id: 1,
+    name: 'Camiseta',
+    price: 29.9,
+    quantity: 3,
+    brands: ['Nike', 'Reebok'],
+  },
+  {
+    id: 2,
+    name: 'Caneca',
+    price: 19.90,
+    quantity: 12,
+    brands: ['Stanley', 'Camelbak'],
+  },
+  {
+    id: 3,
+    name: 'Boné',
+    price: 39.90,
+    quantity: 0,
+    brands: [],
+  },
+];
+
+export default function findById(id: number): Product | string {
+  const produt = products.find(product => product.id === id)
+  if(!produt) return `Produto com ID ${id} não encontrado.`
+  return produt
+}
