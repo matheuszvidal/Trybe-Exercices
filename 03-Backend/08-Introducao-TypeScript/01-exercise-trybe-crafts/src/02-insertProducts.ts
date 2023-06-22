4ba5ae81afca8f9af243eff193aea862d0d06b37
Matheus Vidal <matheusvidalpro@gmail.com>
2023-06-21 23:11:43 -0400

    feat: requirement 02 - done

---
+++ 03-Backend/08-Introducao-TypeScript/01-exercise-trybe-crafts/src/02-insertProducts.ts
@@ -0,0 +1,13 @@
+export type Product = {
+  id: number,
+  name: string,
+  price: number,
+  quantity: number,
+  brands: string[]
+}
+
+export default function insertProducts(brand: string[], product: Product): string {
+  const checkBrandInArray = brand.every(item => product.brands.includes(item))
+  if (checkBrandInArray) return `${product.name} adicionado(a) com o preço de R$ ${product.price}`
+  return 'Seu produto contém marcas indisponíveis.'
+}
